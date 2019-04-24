3de810c61027f45bdb3c9af42723486f1a76a5f7
Jon Gjengset <jon@thesquareplanet.com>
2019-04-24 13:23:08 -0400

    Mention macOS/BSD support in README/docs

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,16 @@
 //! rather than a separate timing mechanism like [`tokio-timer`]. This comes at somewhat increased
 //! overhead if you have many timers, but allows the timers to have any granularity supported by
 //! your operating system where `tokio-timer` can only support timers with a granularity of 1ms.
+//! In particular, the system timers usually support whatever granularity the underlying hardware
+//! supports (see "High-resolution timers" in [`time(7)`]), which on my laptop is 1ns!
 //!
-//! The current implementation relies on [`timerfd_create(2)`], and will thus only work on
-//! platforms whose `libc` contains that call (probably just Linux at the moment).
+//! The current implementation uses [`timerfd_create(2)`] on Linux, and [`kqueue(2)` timers] on
+//! macOS and BSDs.
 //!
 //!   [`tokio-timer`]: https://docs.rs/tokio-timer/
 //!   [`timerfd_create(2)`]: https://linux.die.net/man/2/timerfd_settime
+//!   [`kqueue(2)` timers]: https://man.openbsd.org/kqueue.2
+//!   [`time(7)`]: https://linux.die.net/man/7/time
 
 #![deny(missing_docs)]
 
